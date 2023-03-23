ec6e7f9e7b428b13541f5996dd0c73950cfaff0d
taoweishu <taoweishu2000@163.com>
2023-03-23 17:27:56 +0800

    :sparkles: 快速排序算法

---
+++ LeetCode/src/com/lc/template/QuickSort.java
@@ -0,0 +1,41 @@
+package com.lc.template;
+
+/**
+ * @Author taoweishu
+ * @Date 2023/03/23 16:59
+ **/
+public class QuickSort {
+	public static void quickSort(int[] nums, int left, int right){
+		if (left > right) {
+			return;
+		}
+		int l = left;
+		int r = right;
+		int p = nums[left];
+		while(l < r) {
+			while(nums[r] >= p && l < r) {
+				r--;
+			}
+			while(nums[l] <= p && l < r) {
+				l++;
+			}
+			swap(nums, l, r);
+		}
+		nums[left] = nums[l];
+		nums[l] = p;
+		quickSort(nums, left, l - 1);
+		quickSort(nums, l + 1, right);
+	}
+	public static void swap(int[] nums, int a, int b) {
+		int temp = nums[a];
+		nums[a] = nums[b];
+		nums[b] = temp;
+	}
+	public static void main(String[] args) {
+		int[] nums = new int[]{8,4,6,3,7,4,2,5,2,1};
+		quickSort(nums, 0, nums.length - 1);
+		for(int n : nums) {
+			System.out.print(n + " ");
+		}
+	}
+}
