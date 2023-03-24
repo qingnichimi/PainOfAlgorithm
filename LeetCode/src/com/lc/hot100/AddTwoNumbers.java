e3c41ef8cef37be3228f150990341224f706c6aa
taoweishu <taoweishu2000@163.com>
2023-03-24 11:08:18 +0800

    :fire: 去掉两数相加的输出代码

--- LeetCode/src/com/lc/hot100/AddTwoNumbers.java
+++ LeetCode/src/com/lc/hot100/AddTwoNumbers.java
@@ -35,18 +35,6 @@ public class AddTwoNumbers {
 
 	}
 
-	public static void main(String[] args) {
-		ListNode l1 = new ListNode(1);
-		l1.next = new ListNode(2);
-		ListNode l2 = new ListNode(2);
-		l2.next = new ListNode(9);
-		ListNode res = addTwoNumbers(l1, l2);
-		while(res.next != null) {
-			System.out.print(res.val);
-			res = res.next;
-		}
-
-	}
 }
 
 class ListNode {
